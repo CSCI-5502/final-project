a92aec2d002a983280f39018c0366e8f06f24b70
dmta9796 <dmta9796@colorado.edu>
2020-07-22 12:19:32 -0600

    basic checks

--- player-style/style_cluster.py
+++ player-style/style_cluster.py
@@ -2,6 +2,7 @@ import os
 import glob
 from pathlib import Path
 
+import json
 import csv
 import numpy as np
 import pandas as pd 
@@ -15,26 +16,55 @@ def tsne_cluster():
 def data_preprocess():
     pass
 
+def checksparseness(df):
+    sparse = dict()
+    sparse["totalrows"] = df.shape[0]
+
+    columns = df.columns.values
+    print("columns: ",columns)
+    for column in columns:
+        columndata = df[column]
+        count = int(columndata.isna().sum())
+        sparse[column] = count
+
+
+    return sparse
+
+def cyclesparsenessfiles(dflist,filenames):
+    resultdict = dict()
+    for [df,filename] in zip(dflist,filenames):
+        result = checksparseness(df)
+        resultdict[str(filename)] = result
+    
+    return resultdict
+
 def read_charting_data(directory):
     dflist = []
+    filenames = []
     for filepath in Path(directory).glob('**/*'):
         afile = filepath.absolute()
         print(afile)
         extension = afile.suffix
-        print(extension)
+        #print(extension)
         if(extension ==".csv"):
             df = pd.read_csv(afile, error_bad_lines=False, engine='python') #, quoting=csv.QUOTE_NONE, encoding='utf-8')
             dflist.append(df)
+            filenames.append(os.path.basename(afile))
 
-    return dflist
+    return dflist,filenames
 
 def main():
     dirname = Path("/home/dima/Desktop/datamining/project/files/tennis_MatchChartingProject")
     files = os.listdir(dirname)
     #print(files)
-    dflist = read_charting_data(dirname)
-    print(dflist)
+    dflist,filenames = read_charting_data(dirname)
+    #print(dflist)
+
+    result = cyclesparsenessfiles(dflist,filenames)
+    print(result)
     #df = pd.read_csv()
+    with open('ChartingProjectSparsity.json', 'w') as outfile:
+        json.dump(result,outfile,indent=4)
 
 if __name__ == "__main__":
     main()
