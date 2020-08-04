7c4052dcbab87e1c12aa0da7b1d9c3f8e38fc073
dmta9796 <dmta9796@colorado.edu>
2020-08-03 19:56:49 -0600

    was too careless cleaning code

--- player-style/style_cluster.py
+++ player-style/style_cluster.py
@@ -10,9 +10,6 @@ import sklearn as sk
 from sklearn.manifold import TSNE
 
 
-def tsne_cluster():
-    pass
-
 
 def aggregatedata(df):
     df = df.sum(level = 'player')
@@ -24,11 +21,8 @@ def data_preprocess(df):
     nameidx = df['player'].astype(str)
     name1 = [name.split('-')[-1] for name in names1]
     name2 = [name.split('-')[-2] for name in names2]
-    #df['name1'] = name1
-    #df['name2'] = name2
     newnameidx = [name1[idx] if player else name2[idx] for (idx,player) in enumerate(nameidx)]
     df['player'] = newnameidx
-    #print(df.head())
     return df 
 
 def getmatches(df):
@@ -210,7 +204,6 @@ def checksparseness(df):
     sparse["totalrows"] = df.shape[0]
 
     columns = df.columns.values
-    #print("columns: ",columns)
     for column in columns:
         columndata = df[column]
         count = int(columndata.isna().sum())
@@ -221,45 +214,27 @@ def checksparseness(df):
 
 
 def getnumericdata(dflist):
-     #[0,2,4,5,8,9,13,16,17,18,20,21,22,23,25,26,29,30,31,32,36]
     dflist[0] = getsvbreaksplit(dflist[0]) # charting-w-stats-SvBreakSplit.csv
-    #dflist[1] = getservebasics(dflist[1]) # charting-w-stats-ServeBasics.csv
     dflist[1] = getreturndepth(dflist[1]) # charting-w-stats-ReturnDepth.csv
-    #dflist[3] = getserveinfluence(dflist[3]) # charting-w-stats-ServeInfluence.csv
     dflist[2] = getshotdirection(dflist[2])# charting-w-stats-ShotDirection.csv
     dflist[3] = getreturnoutcome(dflist[3])# charting-m-stats-ReturnOutcomes.csv
-    #dflist[6] = getserveDirection(dflist[6])# charting-m-stats-ServeDirection.csv
-    #dflist[7] = getserveDirection(dflist[7])# charting-w-stats-ServeDirection.csv
     dflist[4] = getshotdir(dflist[4])# charting-m-stats-ShotDir.csv
     dflist[5] = getshotdirection(dflist[5]) #charting-m-stats-ShotDirection.csv
-    #dflist[10] = getkeypointserve(dflist[10]) # charting-m-stats-KeyPointsServe.csv
-    #dflist[11] = getkeypointreturn(dflist[11]) # charting-m-stats-KeyPointsReturn.csv
-    #dflist[12] = getservebasics(dflist[11]) # charting-m-stats-ServeBasics.csv
     dflist[6] = getnetpoints(dflist[6]) # charting-m-stats-NetPoints.csv
-    #dflist[14] = getmatches(dflist[13]) # charting-m-matches.csv
-    #dflist[15] = getserveinfluence(dflist[14]) # charting-m-stats-ServeInfluence.csv
     dflist[7] = getsvBreakTotal(dflist[7])# charting-w-stats-SvBreakTotal.csv
     dflist[8] = getreturndepth(dflist[8])# charting-m-stats-ReturnDepth.csv
     dflist[9] = getoverview(dflist[9])# charting-w-stats-Overview.csv
-    #dflist[19] = getpoints(dflist[18])# charting-w-points.csv
     dflist[10] = getshotdiroutcomes(dflist[10])# charting-m-stats-ShotDirOutcomes.csv
     dflist[11] = getsnv(dflist[11])# charting-w-stats-SnV.csv
     dflist[12] = getsnv(dflist[12])# charting-m-stats-SnV.csv
     dflist[13] = getnetpoints(dflist[13])# charting-w-stats-NetPoints.csv
-    #dflist[24] = getmatches(dflist[23])# charting-w-matches.csv
     dflist[14] = getreturnoutcome(dflist[14])# charting-w-stats-ReturnOutcomes.csv
     dflist[15] = getsvbreaksplit(dflist[15])# charting-m-stats-SvBreakSplit.csv
-    #dflist[27] = getkeypointserve(dflist[26])# charting-w-stats-KeyPointsServe.csv
-    #dflist[28] = getkeypointreturn(dflist[27])# charting-w-stats-KeyPointsReturn.csv
     dflist[16] = getshottypes(dflist[16])# charting-w-stats-ShotTypes.csv
     dflist[17] = getsvBreakTotal(dflist[17])# charting-m-stats-SvBreakTotal.csv
     dflist[18] = getshottypes(dflist[18])# charting-m-stats-ShotTypes.csv
     dflist[19] = getshotdiroutcomes(dflist[19]) # charting-w-stats-ShotDirOutcomes.csv
-    #dflist[33] = getrally(dflist[33])# charting-m-stats-Rally.csv
-    #dflist[34] = getpoints(dflist[34])# charting-m-points.csv
-    #dflist[35] = getrally(dflist[35])# charting-w-stats-Rally.csv
     dflist[20] = getoverview(dflist[20])# charting-m-stats-Overview.csv
-    #[0,2,4,5,8,9,13,16,17,18,20,21,22,23,25,26,29,30,31,32,36]
     return dflist
 
 def cyclesparsenessfiles(dflist,filenames):
@@ -276,17 +251,13 @@ def read_charting_data(directory):
     filenames = []
     for filepath in Path(directory).glob('**/*'):
         afile = filepath.absolute()
-        #print(afile)
         extension = afile.suffix
-        #print(extension)
         if(extension ==".csv"):
             print(afile)
-            df = pd.read_csv(afile, engine='python',index_col=False) #,error_bad_lines=False) #, quoting=csv.QUOTE_NONE, encoding='utf-8')
+            df = pd.read_csv(afile, engine='python',index_col=False)
             df.rename( columns={0 :'id'}, inplace=True)
             dflist.append(df)
             filenames.append(os.path.basename(afile))
-            #if(filenames[-1] == "charting-m-stats-ReturnOutcomes.csv"): #"charting-m-stats-ReturnOutcomes.csv"):
-                #print(df)
 
     return dflist,filenames
 
@@ -294,19 +265,11 @@ def read_charting_data(directory):
 def main():
     dirname = Path("/home/dima/Desktop/datamining/project/files/tennis_MatchChartingProject")
     files = os.listdir(dirname)
-    #print(files)
     dflist,filenames = read_charting_data(dirname)
-    #print(dflist)
-
-    #result = cyclesparsenessfiles(dflist,filenames)
-    #print(result)
 
     indexes = [0,2,4,5,8,9,13,16,17,18,20,21,22,23,25,26,29,30,31,32,36]
-    #print("indexes: ",indexes, indexes.shape)
     newdflist = [dflist[idx] for idx in indexes]
     dflist = newdflist
-    #print('test: ',dflist)
-    #print(dflist[0].head())
 
     for idx,_ in enumerate(dflist):
         print('index: ',idx)
